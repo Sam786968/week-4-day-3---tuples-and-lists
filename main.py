2648d22b31bcd102412baeff64ad2ab2238210e4
Sam Zajczenko <113551869+Sam786968@users.noreply.github.com>
2022-09-16 19:08:51 +0000

    index and lists review

--- main.py
+++ main.py
@@ -4,10 +4,13 @@
 ##############################################################################################################
 # basic review
 # Find and display on the screen which character occupies the fifth position within the following word:
-"computer"
+# computer = "computer"
+# print(computer[4])
+# print(computer[-1])
 
-# Find and display the index of the last occurrence of the word "practice" in the following sentence:
-"In theory, theory and practice are the same. In practice, they are not."
+# # Find and display the index of the last occurrence of the word "practice" in the following sentence:
+# newSentence = "In theory, theory and practice are the same. In practice, they are not."
+# print(newSentence.rindex("practice"))
 
 ###############################################Review############################################################
 #####################################challenge 1################################################################
